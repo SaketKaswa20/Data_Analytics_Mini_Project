56473f8c6b7fa38b0803047c75147238e6784b38
Saket Kaswa <105808363+SaketKaswa20@users.noreply.github.com>
2023-11-15 15:36:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -146,8 +146,7 @@ df.interpolate()
 df=df.dropna()
 
 """#Applying Linear Regression"""
-pip install scikit-learn
-import sklearn
+import scikit-learn
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
