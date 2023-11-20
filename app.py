ab590286c8dc9856742633a1c635f6f178db9f3e
Saket Kaswa <105808363+SaketKaswa20@users.noreply.github.com>
2023-11-20 21:21:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -149,36 +149,36 @@ st.write("Drop Rows with Missing Values:")
 df_no_missing = df.dropna()
 st.write(df_no_missing)
 
-"""#Applying Linear Regression"""
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+# """#Applying Linear Regression"""
+# from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LinearRegression
+# from sklearn.metrics import mean_squared_error
 
-# Step 2: Data Preprocessing
-# For simplicity, let's consider 'Close' as the target variable and 'Open' as the feature
-X = df['Open'].values.reshape(-1, 1)  # Feature
-y = df['Close'].values  # Target
+# # Step 2: Data Preprocessing
+# # For simplicity, let's consider 'Close' as the target variable and 'Open' as the feature
+# X = df['Open'].values.reshape(-1, 1)  # Feature
+# y = df['Close'].values  # Target
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# # Split the data into training and testing sets
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Step 3: Apply Simple Linear Regression
-# Create and fit the model
-model = LinearRegression()
-model.fit(X_train, y_train)
+# # Step 3: Apply Simple Linear Regression
+# # Create and fit the model
+# model = LinearRegression()
+# model.fit(X_train, y_train)
 
-# Make predictions
-y_pred = model.predict(X_test)
+# # Make predictions
+# y_pred = model.predict(X_test)
 
-# Step 4: Evaluate the model
-mse = mean_squared_error(y_test, y_pred)
-print(f'Mean Squared Error: {mse}')
+# # Step 4: Evaluate the model
+# mse = mean_squared_error(y_test, y_pred)
+# print(f'Mean Squared Error: {mse}')
 
-# Step 5: Visualize the results
-plt.scatter(X_test, y_test, color='black', label='Actual')
-plt.plot(X_test, y_pred, color='blue', linewidth=3, label='Linear Regression')
-plt.title('Simple Linear Regression')
-plt.xlabel('Open Price')
-plt.ylabel('Close Price')
-plt.legend()
-plt.show()
+# # Step 5: Visualize the results
+# plt.scatter(X_test, y_test, color='black', label='Actual')
+# plt.plot(X_test, y_pred, color='blue', linewidth=3, label='Linear Regression')
+# plt.title('Simple Linear Regression')
+# plt.xlabel('Open Price')
+# plt.ylabel('Close Price')
+# plt.legend()
+# plt.show()
